ab3bd14c38159c73317297671454bee8002b0a68
daso <daso@itu.dk>
2021-10-01 13:28:29 +0200

    Kanban Board implementation (Exercise 1-5 maybe)

--- Assignment4.Entities/Task.cs
+++ Assignment4.Entities/Task.cs
@@ -1,6 +1,26 @@
+
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using Assignment4.Core;
+
 namespace Assignment4.Entities
 {
     public class Task
     {
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        public User AssignedTo { get; set; }
+
+        public string Descrption { get; set; }
+
+        [Required]
+        public State State { get; set; }
+
+        public IEnumerable<Tag> Tags { get; set; }
     }
 }
--- Assignment4.Entities/User.cs
+++ Assignment4.Entities/User.cs
@@ -1,6 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
 namespace Assignment4.Entities
 {
     public class User
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [EmailAddress]
+        [Required]
+        [StringLength(100)]
+        [Key]
+        public string Email { get; set; }
+
+        public IEnumerable<Task> Tasks { get; set; }
     }
 }
